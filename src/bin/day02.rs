9b85fcf1f232d04051b04e8c96f35b3ae68c2e80
Andreas Hartel <ahartel@gmx.de>
2021-12-02 21:56:51 +0100

    Completed part 2 of day02

--- src/bin/day02.rs
+++ src/bin/day02.rs
@@ -4,6 +4,11 @@ use aoc21::load_data;
 use std::iter::Sum;
 
 
+struct Command {
+    command: String,
+    amount: i32,
+}
+
 struct Move {
     horizontal : i32,
     vertical : i32,
@@ -33,16 +38,26 @@ impl<'a> Sum<&'a Self> for Move {
     }
 }
 
-impl Move {
-    fn new(s : &str) -> Move {
+impl Command {
+    fn new(s : &str) -> Command {
         let mut raw_move = s.split(" ");
-        let direction = raw_move.next().unwrap();
+        let direction = String::from(raw_move.next().unwrap());
+        let amount : i32 = raw_move.next().unwrap().parse().unwrap();
+        Command {
+            command: direction,
+            amount: amount
+        }
+    }
+}
+
+impl Move {
+    fn new(c : &Command) -> Move {
         let mut struct_move : Move = Move { horizontal: 0, vertical: 0};
-        match direction {
-            "forward" => struct_move.horizontal = raw_move.next().expect("Split failed").parse().expect("Cast failed"),
-            "down" => struct_move.vertical = raw_move.next().unwrap().parse().unwrap(),
+        match c.command.as_str() {
+            "forward" => struct_move.horizontal = c.amount,
+            "down" => struct_move.vertical = c.amount,
             "up" => {
-                struct_move.vertical = raw_move.next().unwrap().parse().unwrap();
+                struct_move.vertical = c.amount;
                 struct_move.vertical *= -1;
             }
             &_ => panic!("Reached unreachable code")
@@ -62,22 +77,36 @@ impl Move {
 //     }
 // }
 
-fn solve_part_1(vec : &Vec<Move>) {
-    let total : Move = vec.iter().sum();
+fn solve_part_1(vec : &Vec<Command>) {
+    let total : Move = vec.into_iter().map(|c| Move::new(c)).sum();
     println!("Horizontal {}, Vertical {}, Product {}",
         total.horizontal,
         total.vertical,
         total.vertical * total.horizontal);
 }
 
-fn solve_part_2(vec : &Vec<Move>) {
-
+fn solve_part_2(vec : &Vec<Command>) {
+    let mut aim : i32 = 0;
+    let mut postition : i32 = 0;
+    let mut depth : i32 = 0;
+    for c in vec {
+        match c.command.as_str() {
+            "forward" => { postition += c.amount; depth += aim * c.amount; },
+            "down" => aim += c.amount,
+            "up" => aim -= c.amount,
+            &_ => panic!("Reached unreachable code")
+        }
+    }
+    println!("Horizontal postition {}, Depth {}, Product {}",
+        postition,
+        depth,
+        postition * depth);
 }
 
 fn main() {
     let filename = "data/day02/input.txt";
 
-    let vec = load_data(filename, Move::new);
+    let vec = load_data(filename, Command::new);
     solve_part_1(&vec);
     solve_part_2(&vec);
 }
