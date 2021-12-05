540c3921908166a9eff6b1082ec640e473048309
Andreas Hartel <ahartel@gmx.de>
2021-12-05 11:11:15 +0100

    Finished day05

--- src/bin/day05.rs
+++ src/bin/day05.rs
@@ -20,15 +20,15 @@ impl Line {
 
 fn solve_part_1(vec : &Vec<Line>) {
     let mut field : Vec<Vec<usize>> = vec![vec![0; 1000]; 1000];
-    let mut num_fields_greate_one = 0;
+    let mut num_fields_greater_one = 0;
     for line in vec.into_iter().filter(|l| l.start.0 == l.end.0 || l.start.1 == l.end.1) {
-        println!("({},{}) -> ({},{})",
-            line.start.0, line.start.1, line.end.0, line.end.1);
+        //println!("({},{}) -> ({},{})",
+        //    line.start.0, line.start.1, line.end.0, line.end.1);
         if line.start.0 == line.end.0 {
             let (start_y, end_y) = if line.start.1 > line.end.1 { (line.end.1, line.start.1) } else { (line.start.1, line.end.1) };
             for y in start_y..end_y+1 {
                 if field[line.start.0][y] == 1 {
-                    num_fields_greate_one += 1;
+                    num_fields_greater_one += 1;
                 }
                 field[line.start.0][y] += 1;
             }
@@ -37,7 +37,7 @@ fn solve_part_1(vec : &Vec<Line>) {
             let (start_x, end_x) = if line.start.0 > line.end.0 { (line.end.0, line.start.0) } else { (line.start.0, line.end.0) };
             for x in start_x..end_x+1 {
                 if field[x][line.start.1] == 1 {
-                    num_fields_greate_one += 1;
+                    num_fields_greater_one += 1;
                 }
                 field[x][line.start.1] += 1;
             }
@@ -50,12 +50,55 @@ fn solve_part_1(vec : &Vec<Line>) {
     //     print!("\n");
     // }
     // println!("");
-    println!("{}", num_fields_greate_one);
+    println!("{}", num_fields_greater_one);
 }
 
 fn solve_part_2(vec : &Vec<Line>) {
-    
-    //println!("o2: {}, co2: {}, product: {}", o2, co2, o2*co2);
+    let mut field : Vec<Vec<usize>> = vec![vec![0; 1000]; 1000];
+    let mut num_fields_greater_one = 0;
+    for line in vec {
+        //println!("({},{}) -> ({},{})",
+        //    line.start.0, line.start.1, line.end.0, line.end.1);
+        if line.start.0 == line.end.0 && line.start.1 != line.end.1 {
+            let (start_y, end_y) = if line.start.1 > line.end.1 { (line.end.1, line.start.1) } else { (line.start.1, line.end.1) };
+            for y in start_y..end_y+1 {
+                if field[line.start.0][y] == 1 {
+                    num_fields_greater_one += 1;
+                }
+                field[line.start.0][y] += 1;
+            }
+        }
+        else if line.start.1 == line.end.1 && line.start.0 != line.end.0 {
+            let (start_x, end_x) = if line.start.0 > line.end.0 { (line.end.0, line.start.0) } else { (line.start.0, line.end.0) };
+            for x in start_x..end_x+1 {
+                if field[x][line.start.1] == 1 {
+                    num_fields_greater_one += 1;
+                }
+                field[x][line.start.1] += 1;
+            }
+        }
+        else {//if (line.start.1-line.end.1).abs() == (line.start.0-line.end.0).abs() {
+            let pos_y = line.start.1 < line.end.1;
+            let pos_x = line.start.0 < line.end.0;
+            let mut x = line.start.0;
+            let mut y = line.start.1;
+            while x != line.end.0 && y != line.end.1 {
+                if field[x][y] == 1 {
+                    num_fields_greater_one += 1;
+                }
+                field[x][y] += 1;
+                x = if pos_x { x+1 } else { x-1 };
+                y = if pos_y { y+1 } else { y-1 };
+            }
+            if x == line.end.0 && y == line.end.1 {
+                if field[x][y] == 1 {
+                    num_fields_greater_one += 1;
+                }
+                field[x][y] += 1;
+            }
+        }
+    }
+    println!("{}", num_fields_greater_one);
 }
 
 fn main() {
@@ -63,5 +106,5 @@ fn main() {
 
     let vec = load_data(filename, Line::new);
     solve_part_1(&vec);
-    //solve_part_2(&vec);
+    solve_part_2(&vec);
 }
